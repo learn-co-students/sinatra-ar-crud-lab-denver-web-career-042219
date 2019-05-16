b88bf0cde33ea4ec5ec1d29b50f738f53b14a4f7
drbarq <J.Tustin@gmail.com>
2019-05-16 13:28:08 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,5 +10,49 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    new = Article.create(title: params[:title], content: params[:content])
+  #create a new object and store it in db while rendering on index
+    redirect "/articles/#{new.id}"
+  end
+
+  get '/articles' do
+    # grab all instances of articles, store in local variable, render them in index
+    @article_all = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    # @render_article = Article.find(params[:id])
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    id = params[:id]
+    title = params[:title]
+    content = params[:content]
+    Article.update(id, :title => title, :content => content)
+
+    redirect "/articles/#{params[:id]}"
+  end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+
+    redirect "/articles"
+  end
+
 end
